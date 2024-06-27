20cdba533dacfcd7639815a9ff71fe132b51b893
openshrine <maxime.d.delannoy@gmail.com>
2024-06-27 10:37:27 +0200

    rm useless files js

--- FrontEnd/article.js
+++
@@ -1,6 +0,0 @@
-class Article{
-    constructor(jsonArticle){
-        jsonArticle && Object.assign(this, jsonArticle);
-    }
-    
-}
--- FrontEnd/articlemanager.js
+++
@@ -1,7 +0,0 @@
-class ArticleManager{
-    constructor(listArticle){
-        this.listArticle = listArticle;
-
-    }
-    
-}
